e6551c8a870231028dda432bd052e084c529c4d9
Steve Harsant <stevenharsant@gmail.com>
2020-08-26 13:35:57 +0100

    V1

--- Start-DevWorkstationSetup.ps1
+++ Start-DevWorkstationSetup.ps1
@@ -1,3 +1,9 @@
+# Check script is run as administrator
+if (! ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544"))) {
+    Write-Output 'Not runnging as administrator. Exiting'
+    exit 1
+}
+
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
@@ -5,6 +11,17 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 
 refreshenv
 
-$packages = @['bitwarden', 'conemu', 'Cura', 'egnyte-connect', 'Firefox', 'git', 'heroku-cli', 'microsoft-teams', 'nodejs', 'powertoys', 'vlc', 'vscode', 'teamviewer', 'zoom']
+# Install Applications
+$packages = 'bitwarden', 'conemu', 'Cura', 'docker', 'egnyte-connect', 'Firefox', 'git', 'grep', 'heroku-cli', `
+            'microsoft-teams', 'nodejs', 'powertoys', 'vlc', 'vscode', 'teamviewer', 'terraform', 'zoom'
 
-for
+foreach ($package in $packages) {
+    choco install $package -y
+}
+
+# Install and update to WSL2
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+# Install alpine linux distro
+choco install wsl-alpine -y
