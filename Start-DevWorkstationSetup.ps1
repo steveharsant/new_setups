70693175d149af4f4596ec34afd717c8a0c0d782
steveharsant <stevenharsant@gmail.com>
2024-03-16 19:48:31 +1100

    updated apps and preferred wsl distro

--- Start-DevWorkstationSetup.ps1
+++ Start-DevWorkstationSetup.ps1
@@ -1,7 +1,7 @@
 # v1.0.0
 
 # Check script is run as administrator
-if (! ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544"))) {
+if (! ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match 'S-1-5-32-544'))) {
     Write-Output 'Not runnging as administrator. Exiting'
     exit 1
 }
@@ -9,21 +9,18 @@ if (! ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 refreshenv
 
 # Install Applications
-$packages = 'bitwarden', 'conemu', 'Cura', 'docker', 'egnyte-connect', 'Firefox', 'git', 'grep', 'heroku-cli', `
-            'microsoft-teams', 'nodejs', 'notion', 'powertoys', 'vlc', 'vscode', 'teamviewer', 'terraform', 'zoom'
+$packages = 'firefox bitwarden conemu docker git grep powertoys vlc vscode ferdium spotify python 7zip tailscale powershell-core'
 
-foreach ($package in $packages) {
-    choco install $package -y
-}
+choco install $packages -y
 
 # Install and update to WSL2
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 # Install alpine linux distro
-choco install wsl-alpine -y
+choco install -y
